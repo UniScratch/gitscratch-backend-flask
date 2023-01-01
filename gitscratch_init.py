f94d69f0edd2a86eda0614fa40f47101387ebda3
wrj2009 <wrj-2009@qq.com>
2023-01-01 22:40:04 +0800

    没有更新
    Signed-off-by: wrj2009 <wrj-2009@qq.com>

--- gitscratch_init.py
+++ gitscratch_init.py
@@ -4,5 +4,5 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 # app.secret_key = str(uuid.uuid4())
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
